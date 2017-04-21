042eba126600b312ae10bb4a4a707b8448ba61ae
Germán Parisi <germannparisi@gmail.com>
2017-04-20 23:41:37 -0300

    Elimino comentarios innecesarios. Agrego .class al gitignore

--- BusquedaPaginas/src/busquedapaginas/BuscadorSitiosWeb.java
+++ BusquedaPaginas/src/busquedapaginas/BuscadorSitiosWeb.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package busquedapaginas;
 
 import java.io.BufferedReader;
@@ -15,48 +10,38 @@ import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *
- * @author 
- */
 public class BuscadorSitiosWeb {
-    
-    
-    public static HashSet buscar(String dominio,int cantidadBusquedas) throws Exception
-    {
+
+    public static HashSet buscar(String dominio, int cantidadBusquedas) throws Exception {
         HashSet<String> s = new HashSet();
-        
+
         boolean bandera = true;
-        int pagina=1;
-        Pattern p = Pattern.compile("(http(s?)://www\\.\\w+\\."+dominio+")");
+        int pagina = 1;
+        Pattern p = Pattern.compile("(http(s?)://www\\.\\w+\\." + dominio + ")");
         Matcher m;
         String respuesta;
-            while(bandera)
-               {
-                   respuesta = BuscadorSitiosWeb.getResponse("https://www.bing.com/search?q=site%3A"+dominio+"&first="+pagina);
-                    m = p.matcher(respuesta);
-                    while(m.find())
-                    {
-                        s.add(m.group().trim());
-                         if(s.size()==cantidadBusquedas)
-                        {
-                            bandera = false;
-                            break;
-                        }
-                    }
-                    
-                    pagina += 10;
-               }
+        while (bandera) {
+            respuesta = BuscadorSitiosWeb.getResponse("https://www.bing.com/search?q=site%3A" + dominio + "&first=" + pagina);
+            m = p.matcher(respuesta);
+            while (m.find()) {
+                s.add(m.group().trim());
+                if (s.size() == cantidadBusquedas) {
+                    bandera = false;
+                    break;
+                }
+            }
+
+            pagina += 10;
+        }
         HashSet<SitioWeb> sitios = new HashSet();
         Iterator it = s.iterator();
-        while(it.hasNext())
-        {
-            sitios.add(new SitioWeb((String)it.next()));
+        while (it.hasNext()) {
+            sitios.add(new SitioWeb((String) it.next()));
         }
         return sitios;
     }
-    
-    private static String getResponse(String paginaBusqueda) throws Exception{
+
+    private static String getResponse(String paginaBusqueda) throws Exception {
         String respuesta = "";
         URL url = new URL(paginaBusqueda);
         URLConnection conn = url.openConnection();
@@ -65,18 +50,15 @@ public class BuscadorSitiosWeb {
         conn.setRequestProperty("Content-Length", "0");
         conn.setRequestProperty("Content-Type", "text/plain;charset=UTF-8");
         conn.setRequestProperty("Accept-Lenguage", "es-AR");
-        conn.setRequestProperty("Connection","close" );
+        conn.setRequestProperty("Connection", "close");
         InputStream res = conn.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(res));
-        while(br.ready())
-        {
-            respuesta +=br.readLine();
+        while (br.ready()) {
+            respuesta += br.readLine();
         }
         br.close();
         res.close();
-        
-    
-    
-    return respuesta;
+
+        return respuesta;
     }
 }
--- BusquedaPaginas/src/busquedapaginas/SitioWeb.java
+++ BusquedaPaginas/src/busquedapaginas/SitioWeb.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package busquedapaginas;
 
-/**
- *
- * @author 
- */
 public class SitioWeb {
     
     private String dominio;
     private boolean seguro;
 
-    
     public SitioWeb(String dominio) {
         this.dominio = dominio;
         
