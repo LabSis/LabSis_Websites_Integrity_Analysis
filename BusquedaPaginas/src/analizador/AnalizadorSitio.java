9b7d873a3d1a4d2bf4b35bd9a6608e7c452d6cac
fdrcbrtl <federicojbertola@gmail.com>
2017-04-24 16:12:58 -0300

    Implemento método para verificar si utiliza CDN

--- BusquedaPaginas/src/analizador/AnalizadorSitio.java
+++ BusquedaPaginas/src/analizador/AnalizadorSitio.java
@@ -4,8 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,11 +28,11 @@ public class AnalizadorSitio {
             is = url.openStream();
             br = new BufferedReader(new InputStreamReader(is));
             while ((linea = br.readLine()) != null) {
-                html.append(linea);                
+                html.append(linea);
             }
-            
-            result = analizar(url,html.toString());
-            
+
+            result = analizar(url, html.toString());
+
         } catch (IOException ex) {
             System.out.printf("URL mal formada: " + url.toString());
         } finally {
@@ -36,57 +41,78 @@ public class AnalizadorSitio {
                     is.close();
                 }
             } catch (IOException ex) {
-               
+
             }
         }
         return result;
     }
 
-    private  static ResultadoSitoWeb analizar(URL url,String html) {
+    private static ResultadoSitoWeb analizar(URL url, String html) {
         ResultadoSitoWeb resultado = new ResultadoSitoWeb(url);
-        
+
         ArrayList<Tag> scriptTags = TagParser.parser(html, new ScriptTag());
-        for(Tag t : scriptTags){
+        for (Tag t : scriptTags) {
             /* Sino tiene definido el src no nos interesa */
-            if(!t.contieneAtributo("src")) continue;
-            
+            if (!t.contieneAtributo("src")) {
+                continue;
+            }
+
             ResultadoTag rt = new ResultadoTag();
             rt.setTagHtml(t.getTagHtml());
-            
+
             /* CDN */
             Tag.Atributo atrSrc = t.getAtributo("src");
-            if(atrSrc != null){
-                boolean usaCdn = usaCdn(atrSrc.getValor());
+            if (atrSrc != null) {
+                boolean usaCdn = usaCdn(url, atrSrc.getValor());
                 rt.setCdn(usaCdn);
             }
-            
+
             /* VERIFICACION DE INTEGRIDAD */
             Tag.Atributo atrIntegridad = t.getAtributo("integrity");
-            if(atrIntegridad != null){
-                rt.setVerificacionIntegridad(true);
+            boolean verificaIntegridad = false;
+            if (atrIntegridad != null) {
+                verificaIntegridad = true;
                 String integrityValue = atrIntegridad.getValor();
                 rt.setTipoVerificacion(tipoVerificacion(integrityValue));
             }
-            
+            rt.setVerificacionIntegridad(verificaIntegridad);
+
             resultado.addResultadoPorTag(rt);
         }
-        
+
         return resultado;
     }
 
-    private static boolean usaCdn(String src) {
-       return false;
+    private static boolean usaCdn(URL url, String src) {
+        boolean usaCdn = false;
+        try {
+            if (src.startsWith("//")) {
+                src = url.getProtocol() + ":" + src;
+            }
+            URI uri = new URI(src);
+            if (uri.isAbsolute()) {
+                String hostUri = uri.getHost();
+                String hostUrl = url.getHost();
+                if (hostUri.equals(hostUrl)) {
+                    usaCdn = false;
+                } else {
+                    usaCdn = true;
+                }
+            }
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(AnalizadorSitio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return usaCdn;
     }
-    
-    private static ResultadoTag.TipoVerificacion tipoVerificacion(String verificacion){
-        if(verificacion.contains("sha384")){
-          return ResultadoTag.TipoVerificacion.SHA_386;
-        }else if(verificacion.contains("sha512")){
+
+    private static ResultadoTag.TipoVerificacion tipoVerificacion(String verificacion) {
+        if (verificacion.contains("sha384")) {
+            return ResultadoTag.TipoVerificacion.SHA_386;
+        } else if (verificacion.contains("sha512")) {
             return ResultadoTag.TipoVerificacion.SHA_512;
-        }else if(verificacion.contains("sha256")){
+        } else if (verificacion.contains("sha256")) {
             return ResultadoTag.TipoVerificacion.SHA_256;
         }
         return null;
     }
-    
 }
