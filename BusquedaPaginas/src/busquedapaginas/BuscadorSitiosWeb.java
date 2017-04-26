8df4de186ef4cc8ffc086839cdd00dc9f0b4cd20
fdrcbrtl <federicojbertola@gmail.com>
2017-04-26 01:00:52 -0300

    Mejoro la busqueda

--- BusquedaPaginas/src/busquedapaginas/BuscadorSitiosWeb.java
+++ BusquedaPaginas/src/busquedapaginas/BuscadorSitiosWeb.java
@@ -1,11 +1,13 @@
 package busquedapaginas;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,7 +24,8 @@ public class BuscadorSitiosWeb {
      * @throws Exception
      */
     public static HashSet<SitioWeb> buscar(String dominio, int cantidadBusquedas) throws Exception {
-        HashSet<String> s = new HashSet();
+        
+        HashSet<SitioWeb> sitios = new HashSet();
 
         boolean bandera = true;
         int pagina = 1;
@@ -30,11 +33,12 @@ public class BuscadorSitiosWeb {
         Matcher m;
         String respuesta;
         while (bandera) {
-            respuesta = BuscadorSitiosWeb.getResponse("https://www.bing.com/search?q=site%3A" + dominio + "&first=" + pagina);
+            respuesta = BuscadorSitiosWeb.getHtml("https://www.bing.com/search?q=site%3A" + dominio + "&first=" + pagina);
             m = p.matcher(respuesta);
             while (m.find()) {
-                s.add(m.group().trim());
-                if (s.size() == cantidadBusquedas) {
+                String urlStr = m.group().trim();
+                sitios.add(new SitioWeb(urlStr));
+                if (sitios.size() == cantidadBusquedas) {
                     bandera = false;
                     break;
                 }
@@ -42,29 +46,34 @@ public class BuscadorSitiosWeb {
 
             pagina += 10;
         }
-        HashSet<SitioWeb> sitios = new HashSet();
-        Iterator<String> it = s.iterator();
-        while (it.hasNext()) {
-            sitios.add(new SitioWeb(it.next()));
-        }
+
         return sitios;
     }
 
-    private static String getResponse(String paginaBusqueda) throws Exception {
-        String respuesta = "";
-        URL url = new URL(paginaBusqueda);
-        URLConnection conn = url.openConnection();
-        conn.setUseCaches(false);
-        conn.setDoOutput(true);
-        conn.setRequestProperty("Content-Length", "0");
-        conn.setRequestProperty("Content-Type", "text/plain;charset=UTF-8");
-        conn.setRequestProperty("Accept-Lenguage", "es-AR");
-        conn.setRequestProperty("Connection", "close");
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-            while (br.ready()) {
-                respuesta += br.readLine();
+    private static String getHtml(String paginaBusqueda) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL url = new URL(paginaBusqueda);
+            URLConnection urlCon = url.openConnection();
+            urlCon.setUseCaches(false);
+            urlCon.setDoOutput(true);
+            urlCon.setRequestProperty("Content-Length", "0");
+            urlCon.setRequestProperty("Content-Type", "text/plain;charset=UTF-8");
+            urlCon.setRequestProperty("Accept-Lenguage", "es-AR");
+            urlCon.setRequestProperty("Connection", "close");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                sb.append(linea);
             }
+            br.close();
+        } catch (MalformedURLException mue) {
+            System.out.println("URL mal formada: " + paginaBusqueda);
+        } catch (IOException ex) {
+            System.out.println("Error al leer " + paginaBusqueda);
         }
-        return respuesta;
+        
+        return sb.toString();
     }
 }
