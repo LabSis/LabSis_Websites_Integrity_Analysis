ab81061c3b77c9246f0c70bb39881fee551cdcbc
Germán Parisi <germannparisi@gmail.com>
2017-04-29 18:59:41 -0300

    Agrego serie de Bing. 1, 9, 19,29....

--- BusquedaPaginas/src/busquedapaginas/BuscadorBing.java
+++ BusquedaPaginas/src/busquedapaginas/BuscadorBing.java
@@ -2,13 +2,14 @@ package busquedapaginas;
 
 public class BuscadorBing extends Buscador {
 
+    private static final int PRIMER_SALTO = 8;
     private static final int SALTO = 10;
     private static final String url = "https://www.bing.com/search";
     private static final String qParam = "q=";
     private static final String startParam = "first=";
 
     private String terminacionDominio;
-    private int pagina;
+    private int pagina = 1;
 
     public BuscadorBing() {
         this.pagina = -SALTO;
@@ -21,7 +22,11 @@ public class BuscadorBing extends Buscador {
 
     @Override
     public String proximaUrl() {
-        this.pagina += SALTO;
+        if (this.pagina == 1) {
+            this.pagina += PRIMER_SALTO;
+        } else {
+            this.pagina += SALTO;
+        }
         String proxUrl = url + "?" + qParam + this.terminacionDominio + "&" + startParam + this.pagina;
         return proxUrl;
     }
